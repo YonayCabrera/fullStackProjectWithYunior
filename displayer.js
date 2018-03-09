4d59b06378d2bb4d4a0f1dc9b0ef00187c6919c6
unknown <yonaycl@gmail.com>
2018-03-09 18:54:10 +0000

    add image of hotel

--- displayer.js
+++ displayer.js
@@ -1,14 +1,13 @@
-
-function apartamentoToHtml( APARTAMENTO){
-
-    var card = `<div class="card" style="width: 18rem;">
-    <img class="card-img-top" src="..." alt="Card image cap">
-    <div class="card-body">
+function apartmentsInHtml(apartamentos) {
+    apartamentos.forEach(apartamento => {
+        $("#App").append(`<div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="" alt="Card image cap">
+    <div class="card-body"><img src= "${apartamento[ 'Hotel Image' ].src}">
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     </div>
-    </div>`;
-}
+    </div>`);
+    });
 
-function getCard(){
-    return card;
+
+    
 }
--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 fetch('/data/london.json')
-            .then(response => response.json())
-            .then(apartamentos => {
-                console.log(apartamentos.slice(0,20));
-            })
+  .then(response => response.json())
+  .then(apartamentos => {
+    console.log(apartamentos.slice(0,20));
+    paintApartments(apartamentos.slice(0, 20));
+  })
+  .catch(errorCallback=>{
+    console.log(errorCallback);
+  })
+
+function paintApartments(apartamentos){
+  apartmentsInHtml(apartamentos);
+}
 
