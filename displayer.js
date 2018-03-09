4c03fd05b32a994c48a8ab0fd408c363ec704973
unknown <yonaycl@gmail.com>
2018-03-09 20:45:34 +0000

    add pages

--- displayer.js
+++ displayer.js
@@ -2,12 +2,18 @@ function apartmentsInHtml(apartamentos) {
     apartamentos.forEach(apartamento => {
         $("#App").append(`<div class="card" style="width: 18rem;">
     <img class="card-img-top" src="" alt="Card image cap">
-    <div class="card-body"><img src= "${apartamento[ 'Hotel Image' ].src}">
+    <div class="card-body"><img src= "${apartamento['Hotel Image'].src}">
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     </div>
     </div>`);
     });
+}
 
-
-    
+function paginationInHTML(apartamentos) {
+    var pages = apartamentos.length / 20;
+    for (var i = 0; i < pages; i++) {
+        $("#items").append(`<li class="page-item">
+                <a class="page-link" id="${i}" onClick="setPage(${i})">${i}</a>
+                            </li>`);
+    }
 }
--- index.js
+++ index.js
@@ -1,14 +1,26 @@
+var apartaments = [];
 fetch('/data/london.json')
   .then(response => response.json())
   .then(apartamentos => {
-    console.log(apartamentos.slice(0,20));
+    apartaments = apartamentos;
+    paginationInHTML(apartamentos);
     paintApartments(apartamentos.slice(0, 20));
   })
-  .catch(errorCallback=>{
+  .catch(errorCallback => {
     console.log(errorCallback);
   })
 
-function paintApartments(apartamentos){
+function paintApartments(apartamentos) {
   apartmentsInHtml(apartamentos);
+}
+
+function setPage(page) {
+  $("#App").html('');
+  page = (page + 1) * 20;
+  findPage(page);
+}
+
+function findPage(page) {
+  paintApartments(apartaments.slice(page - 20, page));
 }
 
